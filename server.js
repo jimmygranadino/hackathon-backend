72fb6ad4e90ab8f0cd89d7b542eb6f88356dea1a
Eliott Davidson <ulthos@gmail.com>
2020-08-12 18:07:12 -0700

    /quotes route now accepts a req.body with a sentiment attribute, selects an appropriate quote from the hardcoded list, and returns it

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-let theseQuotes = [{id: 0, quote: "Cool Beans", author: "Nick Quandt", sentiment: "positive"}, {id: 1, quote: "Arf!", author: "the neighbor's dog", sentiment: "positive"}];
-let nextId = 0;
+let quotes = [
+    {id: 0, quote: "Cool Beans", author: "Nick Quandt", sentiment: "positive"}, 
+    {id: 1, quote: "Arf!", author: "the neighbor's dog", sentiment: "positive"}, 
+    {id: 3, quote: "Pain builds character!", author: "abusive parents", sentiment: "negative"}, 
+    {id: 4, quote: "The darkest hour is just before the dawn", author: "Thomas Fuller", sentiment: "negative"}
+];
 
 require("dotenv").config();
 const express = require('express');
@@ -11,11 +15,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send("In base route");
+    res.send("Landing page");
 })
 
-app.get('/quotes', (req,res) => {
-    res.json({data: theseQuotes});
+app.get('/quotes', (req,res) => { 
+    // receives a request with a 'sentiment' body attribute
+    // 'sentiment' can be 'positive', 'negative', or empty
+    // response delivers a random quote from a filtered or unfiltered list, as appropriate 
+    let filteredQuotes = []; 
+    if (req.body.sentiment){
+        quotes.forEach(quote => {
+            if (req.body.sentiment === quote.sentiment){
+                filteredQuotes.push(quote);
+            }
+        })
+    } else {
+        filteredQuotes = quotes
+    }
+    res.json(filteredQuotes[Math.floor(Math.random()*filteredQuotes.length)]);
 })
 
 app.listen(process.env.PORT || 3001, () => console.log(`Server is running on ${process.env.PORT} or 3001`));
